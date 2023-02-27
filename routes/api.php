61fdc1b64acb7826154346151a50dd559748bf98
Terry McCann <terrymccann421@gmail.com>
2023-02-27 17:03:29 -0500

    updated

--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,6 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+
 });
